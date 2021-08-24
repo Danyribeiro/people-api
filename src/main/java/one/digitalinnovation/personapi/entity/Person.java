3d62411bd68ee799a9b5486ccfe1d2eee5b2bbd9
Danielly <dany.s.ribeiro@gmail.com>
2021-08-23 21:34:38 -0300

    New files

---
+++ src/main/java/one/digitalinnovation/personapi/entity/Person.java
@@ -0,0 +1,21 @@
+package one.digitalinnovation.personapi.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Person {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String cpf;
+    private LocalDate birthDate;
+    private  List<Phone> phones;
+}
